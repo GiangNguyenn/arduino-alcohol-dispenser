e1b28dfd118ae804773e5b1560283365598e1c04
GiangNguyen <giang.nguyen@skedulo.com>
2021-01-25 16:55:39 +0700

    Added comments for explanation

--- Server/update_values.php
+++ Server/update_values.php
@@ -3,18 +3,17 @@
 
 $servername = "localhost";
 
-// REPLACE with your Database name
+// with your Database name
 $dbname = "id15889682_arduino_hand_sanitizer";
-// REPLACE with Database user
+// Database user
 $username = "id15889682_arduino";
-// REPLACE with Database user password
+// Database password
 $password = "!?}kun-0C/C3mLd{";
 
 // Keep this API Key value to be compatible with the ESP32 code provided in the project page. 
-// If you change this value, the ESP32 sketch needs to match
 $api_key_value = "tPmAT5Ab3j7F9";
 
-$api_key= $level = "";
+$api_key= $level = ""; //initiate empty strings to take in url values
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $api_key = test_input($_POST["api_key"]);
@@ -23,13 +22,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Create connection
         $conn = new mysqli($servername, $username, $password, $dbname);
+
         // Check connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         } 
         
         $sql = "INSERT INTO liquid_level (level)
-        VALUES ('" . $level . "')";
+        VALUES ('" . $level . "');"; //Insert into table a new row
         
         if ($conn->query($sql) === TRUE) {
             echo "New record created successfully";
@@ -41,18 +41,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $conn->close();
     }
     else {
-        echo "Wrong API Key provided.";
+        echo "Wrong API Key provided."; //catch error for debugging purpose
     }
 
 }
 else {
-    echo "No data posted with HTTP POST.";
+    echo "No data posted with HTTP POST.";  //catch error for debugging purpose
 }
 
 function test_input($data) {
     $data = trim($data);
     $data = stripslashes($data);
-    $data = htmlspecialchars($data);
+    $data = htmlspecialchars($data);  // handle data
     return $data;
 }
 
